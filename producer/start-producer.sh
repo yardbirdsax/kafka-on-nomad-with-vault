36e999c3c904f2ae7ac87657f554ab7be0b14272
Joshua Feierman <josh@sqljosh.com>
2021-03-11 11:33:57 -0500

    The producer start-up script has been formatted for better readability.

--- producer/start-producer.sh
+++ producer/start-producer.sh
@@ -6,4 +6,12 @@ set -e
 cat /opt/kafka/config/producer-template.properties | envsubst | sed 's/\\n/\n/g' > /opt/kafka/config/producer.properties
 
 # Start Kafka producer
-/opt/kafka/bin/kafka-producer-perf-test.sh --topic producer-topic --throughput $KAFKA_PRODUCER_THROUGHPUT --num-records $KAFKA_PRODUCER_NUM_RECORDS --producer.config /opt/kafka/config/producer.properties --record-size 100
+while true
+do
+  /opt/kafka/bin/kafka-producer-perf-test.sh \
+    --topic producer-topic \
+    --throughput $KAFKA_PRODUCER_THROUGHPUT \
+    --num-records $KAFKA_PRODUCER_NUM_RECORDS \
+    --producer.config /opt/kafka/config/producer.properties \
+    --record-size 100
+done
